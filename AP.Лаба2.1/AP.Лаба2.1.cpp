3c28c69132233373813d0d03ebbe0d7ed6925867
MarkStadnytskyi <72573640+MarkStadnytskyi@users.noreply.github.com>
2020-10-24 21:02:20 +0300

    Add files via upload

---
+++ AP.Лаба2.1/AP.Лаба2.1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double m;
+
+	double z1;
+	double z2; 
+	cout << "m = ";
+	cin >> m;
+
+
+	z1 = (1 - 0.25 * pow(sin(2 * m), 2) + cos(2 * m));
+	z2 = pow(cos(m), 2) + pow(cos(m), 4);
+	
+	cout << z1 << endl;
+	cout << z2 << endl;
+
+	cin.get();
+	return 0;
+};
